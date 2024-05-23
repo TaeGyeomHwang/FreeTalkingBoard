65c2ead33898284321db31ade1d26d95764729bf
Lee Seong Jong <dltjdwhd1235@naver.com>
2024-05-23 17:51:18 +0900

    관리자에 의한 계정 정지 기능 구현

--- src/main/resources/static/js/deleteUser.js
+++ src/main/resources/static/js/deleteUser.js
@@ -1,18 +1,23 @@
 const deleteUser = document.getElementById("delete-user");
 
-if(deleteUser){
-    deleteUser.addEventListener("click",(event) => {
-        fetch("/deleteMember",{
-            method : "PUT",
-            headers: {
-                "Content-Type" : "application/json",
-            },
-            body : JSON.stringify({
-                email:document.getElementById("userEmails").value,
-                isDeleted:1,
-            }),
-        }).then(()=>{
-            alert("사용자 정지 완료했습니다.");
+document.addEventListener("DOMContentLoaded", function () {
+    const deleteUser = document.getElementById("delete-user");
+
+    if (deleteUser) {
+        deleteUser.addEventListener("click", (event) => {
+            fetch("/deleteMember", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: document.getElementById("userEmails").value,
+                    isDeleted: 1,
+                }),
+            }).then(() => {
+                alert("사용자 정지 완료했습니다.");
+                window.location.href = '/';
+            });
         });
-    });
-}
+    }
+});
