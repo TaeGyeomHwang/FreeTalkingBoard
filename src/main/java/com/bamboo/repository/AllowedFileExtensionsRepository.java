e526f25d53f4c0e691040449d76ecb5bb0391215
Lee Seong Jong <dltjdwhd1235@naver.com>
2024-05-23 09:14:37 +0900

    파일 확장자 기본값 추가

--- src/main/java/com/bamboo/repository/AllowedFileExtensionsRepository.java
+++ src/main/java/com/bamboo/repository/AllowedFileExtensionsRepository.java
@@ -21,6 +21,9 @@ public interface AllowedFileExtensionsRepository extends JpaRepository<AllowedFi
 
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO allowed_file_extension(file_config_id,extension) values(1,'jpg')",nativeQuery = true)
+    @Query(value = "INSERT INTO allowed_file_extension(file_config_id,extension) values(1,'mp4'),(1,'avi'),(1,'mkv'),(1,'mpg')" +
+            ",(1,'doc'),(1,'docx'),(1,'xls'),(1,'xlsx'),(1,'ppt'),(1,'pptx'),(1,'pdf'),(1,'txt'),(1,'csv'),(1,'json'),(1,'rtf'),(1,'odt')" +
+            ",(1,'zip'),(1,'tar.gz'),(1,'rar')" +
+            ",(1,'jpeg'),(1,'jpg'),(1,'png'),(1,'gif')",nativeQuery = true)
     void defualtExtensions();
 }
